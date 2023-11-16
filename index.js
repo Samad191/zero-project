0532c13d7b5deb2411b7ae031648510ab4cb7477
Samad191 <samad13354@gmail.com>
2023-11-16 14:29:07 +0500

    feat: added new contract address and returning tx response on the frontend

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 
 app.use(express.json());
 
-const contractAddress = "0x08C9e26A45e1Ad3660d474f54811118693AF6Bdc";
+const contractAddress = "0xE78ef47FeEDf1e472C21B9781E83F7A45e1a8C36";
 
 const abi = [
   {
@@ -240,6 +240,44 @@ const abi = [
     type: "function",
   },
   {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "nonce",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "message",
+        type: "bytes32",
+      },
+    ],
+    name: "prefixMessage",
+    outputs: [
+      {
+        internalType: "bytes",
+        name: "",
+        type: "bytes",
+      },
+    ],
+    stateMutability: "pure",
+    type: "function",
+  },
+  {
     inputs: [],
     name: "symbol",
     outputs: [
@@ -269,7 +307,7 @@ const abi = [
     inputs: [
       {
         internalType: "address",
-        name: "recipient",
+        name: "to",
         type: "address",
       },
       {
@@ -293,12 +331,12 @@ const abi = [
     inputs: [
       {
         internalType: "address",
-        name: "sender",
+        name: "from",
         type: "address",
       },
       {
         internalType: "address",
-        name: "recipient",
+        name: "to",
         type: "address",
       },
       {
@@ -355,32 +393,65 @@ app.get("/", (req, res) => {
 });
 
 app.post("/submitSig", async (req, res) => {
-  const { from, to, amount, signature } = req.body;
-  const privateKey = process.env.PRIVATE_KEY;
+  try {
+    const { from, to, amount, signature } = req.body;
+    console.log("params", from, to, amount, signature);
+    const privateKey = process.env.PRIVATE_KEY;
+    const provider = new ethers.JsonRpcProvider(process.env.INFURA_URL);
+
+    const wallet = new ethers.Wallet(privateKey, provider);
+    const contract = new ethers.Contract(contractAddress, abi, wallet);
+
+    const data = contract.interface.encodeFunctionData("transferWithPermit", [
+      from,
+      to,
+      amount,
+      signature,
+    ]);
+    const tx = {
+      to: contractAddress,
+      data: data,
+    };
+
+    console.log("send the tx");
+
+    const response = await wallet.sendTransaction(tx);
+    console.log("Transaction hash:", response.hash);
+    await response.wait();
+
+    console.log("done =====");
+    res.send(response).status(200);
+  } catch (err) {
+    res.send("error");
+  }
+});
+
+app.put("/getFunds", async (req, res) => {
+  const { recipient, amount } = req.body;
+  console.log("get funds working");
+
+  const privateKey = process.env.FUNDS_PRIVATE_KEY;
+  console.log("funds private key", privateKey);
   const provider = new ethers.JsonRpcProvider(process.env.INFURA_URL);
 
   const wallet = new ethers.Wallet(privateKey, provider);
   const contract = new ethers.Contract(contractAddress, abi, wallet);
 
-  const data = contract.interface.encodeFunctionData("transferWithPermit", [
-    from,
-    to,
+  const data = contract.interface.encodeFunctionData("transfer", [
+    recipient,
     amount,
-    signature,
   ]);
+
   const tx = {
     to: contractAddress,
     data: data,
   };
 
-  console.log("send the tx");
-
   const response = await wallet.sendTransaction(tx);
   console.log("Transaction hash:", response.hash);
   await response.wait();
 
-  console.log("done =====");
-  res.send("hello").status(200);
+  res.send("Success").status(200);
 });
 
 app.put("/getMsgHashFromFinalHash", (req, res) => {
@@ -390,4 +461,4 @@ app.put("/getMsgHashFromFinalHash", (req, res) => {
   res.send({ messageHash });
 });
 
-app.listen(port, "0.0.0.0");
+app.listen(port, "0.0.0.0", () => console.log("server running"));
